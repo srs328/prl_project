f57774a8d442eed5d116471cc3acee688a10b358
srs328 <srs32896@gmail.com>
2025-05-29 14:19:36 -0400

    train swin and segresnet

--- training/test_train1/run_networks.sh
+++ training/test_train1/run_networks.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+python /home/srs-9/Projects/prl_project/training/test_train0/train_segresnet.py
 python /home/srs-9/Projects/prl_project/training/test_train1/train_swinunet.py
 python /home/srs-9/Projects/prl_project/training/test_train1/train_segresnet.py
