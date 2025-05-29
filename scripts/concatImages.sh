a4cd0fa37f06946d9eceeef8a3058ec91e141bac
srs328 <srs32896@gmail.com>
2025-05-29 12:38:37 -0400

    trained first

--- scripts/concatImages.sh
+++ scripts/concatImages.sh
@@ -4,6 +4,10 @@ work_dir=$1
 
 cd $work_dir || exit
 
-if [[ ! -f flair.phase.t1.nii.gz ]]; then
-    fslmerge -a flair.phase.t1.nii.gz flair.nii.gz phase.nii.gz t1.nii.gz
+# if [[ ! -f flair.phase.t1.nii.gz ]]; then
+#     fslmerge -a flair.phase.t1.nii.gz flair.nii.gz phase.nii.gz t1.nii.gz
+# fi
+
+if [[ ! -f flair.phase.nii.gz ]]; then
+    fslmerge -a flair.phase.nii.gz flair.nii.gz phase.nii.gz
 fi
---
+++ training/test_train1/create_labels.py
@@ -0,0 +1,28 @@
+import subprocess
+from pathlib import Path
+import json
+
+with open("/home/srs-9/Projects/prl_project/data/subject-sessions-longit.json", 'r') as f:
+    subject_sessions = json.load(f)
+
+with open("/home/srs-9/Projects/prl_project/training/test_train0/subjects.txt", 'r') as f:
+    subjects = [line.strip() for line in f.readlines()]
+
+dataroot = Path("/media/smbshare/3Tpioneer_bids")
+script = "/home/srs-9/Projects/prl_project/training/test_train0/createLabels.sh"
+
+with open("errors.log", 'w') as f:
+    for subid in subjects:
+        sessions = sorted(subject_sessions[subid])
+        work_dir = dataroot / f"sub-ms{subid}" / f"ses-{sessions[0]}"
+        cmd = ["bash", script, work_dir]
+        print([str(item) for item in cmd])
+
+        try:
+            result = subprocess.run(cmd, check=True, capture_output=True)
+        except subprocess.CalledProcessError as e:
+            print(subid)
+            print("Stderr:", e.stderr)
+            f.write(subid + "\n" + str(e.stderr) + "\n\n")
+            continue
+
